04c70e77e2dcfb7694abfb7e02b708df40b328ab
m.aantjes <65569213+MaxAantjes@users.noreply.github.com>
2020-06-24 14:55:31 -0500

    Created Plot 3
    Wrote code to create plot 3.

---
+++ plot3.R
@@ -0,0 +1,32 @@
+library(chron, dplyr)
+
+## Load file into R
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+td = tempdir()
+temp = tempfile(tmpdir=td, fileext=".zip")
+download.file(url,temp)
+unzip(temp, overwrite=TRUE)
+dat0 <- read.csv("household_power_consumption.txt", sep = ";",
+                 na.strings = "?")
+unlink(temp)
+
+## Convert variables to the right class. 
+dat1 <- transform(dat0, Date = as.Date(Date, format = "%d/%m/%Y"))
+dat2 <- transform(dat1, Time = times(Time))
+
+## Select the rows of interest
+dat3 <- subset(dat2, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+## Create plot
+png(file = "plot3.png", width = 480, height = 480, units = "px")
+dat3$date.and.time <- as.POSIXct(paste(dat3$Date, dat3$Time), 
+                                 format = "%Y-%m-%d %H:%M:%S")
+plot(dat3$date.and.time, dat3$Sub_metering_1, type = "l",
+     xlab = "", ylab = "Energy sub metering")
+lines(dat3$date.and.time, dat3$Sub_metering_2, type = "l",
+     xlab = "", ylab = "Energy sub metering", col = "red")
+lines(dat3$date.and.time, dat3$Sub_metering_3, type = "l",
+      xlab = "", ylab = "Energy sub metering", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1, 1, 1), col = c("black", "red", "blue"))
+dev.off()
